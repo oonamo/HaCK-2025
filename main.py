28c8a78b30cdf7081115e9d368add1541f088962
rjsjovanna4 <jovannarojas@Jovannas-MacBook-Pro.local>
2025-07-25 14:18:56 -0700

    Update main.py and add config files

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ ldr = ADC(Pin(26))  # GP26 = ADC0
 
 def get_voltage():
     raw = ldr.read_u16()
-    voltage = (raw / 65535) * 3.3  # Convert to volts
+    voltage = (raw / 65535) * 3.3  # Converts to volts
     return round(voltage, 3)
 
 while True:
     print("Voltage:", get_voltage(), "V")
     time.sleep(5)
+    
