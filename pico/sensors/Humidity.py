b31e7dbf79c69b780186a747c1962a3459aabe29
Andrey A <andreyangos123@gmail.com>
2025-07-25 12:23:38 -0700

    fixed while loop and temp conversion to fahrenheit

--- pico/sensors/Humidity.py
+++ pico/sensors/Humidity.py
@@ -5,21 +5,12 @@ from time import sleep
 
 sensor = dht.DHT11(Pin(22))
 
-while True:
-    try:
-        sleep(2)
-        sensor.measure()
-        temperature = sensor.temperature()
-        humidity = sensor.humidity()
-        print('Temperature: %3.1f C' %temperature)
-        print('Humidity: %3.1f %%' %humidity)
-    except OSError as e:
-        print('Failed to read sensor.')
 
 def get_temperature():
     try:
         sensor.measure()
-        return sensor.temperature()
+        tempF = (sensor.temperature() * (9 / 5)) + 32
+        return tempF
     except OSError as e:
         print('Failed to read temperature.')
         return None
