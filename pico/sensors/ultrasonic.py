17c648e5c5b6908a192a318cbf3c296227aeacc5
oonamo <onam75hernandez@gmail.com>
2025-07-26 01:20:16 -0700

    handle exception in ultrasonic sensor

--- pico/sensors/ultrasonic.py
+++ pico/sensors/ultrasonic.py
@@ -6,4 +6,7 @@ ECHO_PIN = 21
 sensor = HCSR04(trigger_pin=TRIGGER_PIN, echo_pin=ECHO_PIN)
 
 def get_distance_cm():
-    return sensor.distance_cm()
+    try:
+        return sensor.distance_cm()
+    except OSError:
+        return None
